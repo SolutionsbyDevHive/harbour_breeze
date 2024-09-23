16fb9809cb2946bb05b12ae3be68b31a63a2347f
SolutionsbyDevHive <devangkh1206@gmail.com>
2024-09-24 00:46:46 +0530

    Initital

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "purple-gradient": "linear-gradient(180deg, #401b4e 0%, #5f286f 100%)",
+        "btn-gradient": "linear-gradient(180deg, #FDE100 0%, #F58921 100%)",
+        "pink-gradient1":
+          "linear-gradient(118deg, #E7278A 0%, rgba(231, 39, 138, 0.00) 100%)",
+        "pink-gradient2":
+          "linear-gradient(118deg, rgba(231, 39, 138, 0.00) 0%, #E7278A 100%)",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
