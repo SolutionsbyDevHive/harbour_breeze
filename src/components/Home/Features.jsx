a053990de4e7c888076a1a91da405ff888acb97b
Devang-Harsora33 <devangkh1206@gmail.com>
2024-09-28 02:39:35 +0530

    update

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -3,18 +3,16 @@ import Photo1 from "/Features/1.webp";
 import Photo2 from "/Features/2.webp";
 import Photo3 from "/Features/3.webp";
 import Photo4 from "/Features/4.webp";
-import AOS from "aos";
-import "aos/dist/aos.css";
 export const Features = () => {
   return (
-    <section className=" w-full mt-12 px-[1rem] md:px-[0rem] flex justify-start flex-col items-center text-blue-950">
-      <div className=" w-full text-center md:text-left pl-[0rem] md:pl-[6rem]">
+    <section className=" w-full px-[1rem] md:px-[0rem] flex justify-start flex-col items-center text-blue-950">
+      <div className=" w-full text-center md:text-left pl-[0rem] md:pl-[6rem] mt-12">
         <span className=" text-3xl text-[#d5b36b] font-montserrat border-b-[1px] border-[#d5b36b]">
           Features of{" "}
         </span>{" "}
         <span className=" text-3xl ml-1 font-montserrat">Pavillion</span>
-      </div>
-      <section className=" py-6 flex flex-col gap-y-6  px-[1rem] md:px-[3rem]">
+      </div>{" "}
+      <section className=" py-6 flex flex-col gap-y-6  px-[1rem] md:px-[0rem]">
         <div className=" flex w-full flex-col md:flex-row justify-center gap-y-1 gap-x-[5rem] items-center md:items-start ">
           <img className=" w-[60%] md:w-[30%]" src={Photo1} alt="photo1" />
           <div className="bg-[#222222] w-[80%] md:w-[50%] h-[20rem] p-4 text-white">
@@ -35,7 +33,7 @@ export const Features = () => {
         </div>
 
         <div className=" flex w-full justify-center px-2 py-1 bg-custom-blue-gradient flex-col md:flex-row gap-y-1 gap-x-[5rem] items-center md:items-start">
-          <div className="  mt-4 text-right hidden md:block w-[70%] md:w-[50%] h-[20rem] ">
+          <div className="  mt-4 text-right hidden md:block w-[70%] md:w-[50%] h-[20rem] text-white">
             <h2 className=" font-serif text-lg ">Architect Speak</h2>
             <p className="mt-8">
               The landmark location makes ‘The Pavilion’ a marquee project. Our
@@ -103,7 +101,7 @@ export const Features = () => {
         </div>
 
         <div className=" flex w-full justify-center bg-custom-blue-gradient flex-col md:flex-row gap-y-1 gap-x-[5rem] items-center md:items-start">
-          <div className="  mt-4 text-right hidden md:block w-[70%] md:w-[50%] h-[20rem] ">
+          <div className="  mt-4 text-right hidden md:block w-[70%] md:w-[50%] h-[20rem] text-white">
             <h2 className=" font-serif text-lg ">Architect Speak</h2>
             <p className="mt-4">
               The landmark location makes ‘The Pavilion’ a marquee project. Our
@@ -126,7 +124,7 @@ export const Features = () => {
             </p>
           </div>
           <img className=" w-[80%] md:w-[40%]" src={Photo4} alt="photo4" />
-          <div className=" text-left md:text-right block md:hidden w-[80%] md:w-[50%] h-auto p-4 ">
+          <div className=" text-left md:text-right block md:hidden w-[80%] md:w-[50%] h-auto p-4 text-white">
             <h2 className=" font-serif text-lg ">Architect Speak</h2>
             <p className="mt-4">
               The landmark location makes ‘The Pavilion’ a marquee project. Our
